6a4f4cc94d7f7c103b40bf1f44ff4cfbc1bdbb0b
george2 <fegati02@gmail.com>
2017-01-04 04:43:40 +0300

    added jpes program, program service and program store

---
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/jphes/program/Program.java
@@ -0,0 +1,18 @@
+package org.hisp.dhis.jphes.program;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import org.hisp.dhis.common.BaseIdentifiableObject;
+import org.hisp.dhis.common.DxfNamespaces;
+
+/**
+ * Created by xenial on 1/4/17.
+ */
+@JacksonXmlRootElement( localName = "dataElement", namespace = DxfNamespaces.DXF_2_0 )
+public class Program extends BaseIdentifiableObject {
+
+    /**
+     * formName is what will be displayed to the user
+     * useful where name is too long
+     */
+    private String formName;
+}
---
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/jphes/program/ProgramStore.java
@@ -0,0 +1,14 @@
+package org.hisp.dhis.jphes.program;
+
+import org.hisp.dhis.common.GenericIdentifiableObjectStore;
+
+import java.util.List;
+
+/**
+ * Created by xenial on 1/4/17.
+ */
+public interface ProgramStore extends GenericIdentifiableObjectStore<Program> {
+    String ID = ProgramStore.class.getName();
+
+    List<Program> searchProgramByName(String key);
+}
