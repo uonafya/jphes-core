08bf9ce691173702530cb466d40edf42f6c99035
banga <dennisbanga@gmail.com>
2017-01-07 23:45:12 +0300

    added validation rules

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/validationRules.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/validationRules.js
@@ -594,5 +594,21 @@ var validationRules = {
         "organisationUnitLevel" : {
             "required" : true
         }
+    },
+//  JPHES Rules
+    "nationalUnit" : {
+        "name" : {
+            "required" : true,
+            "rangelength" : [ 2, 230 ]
+        },
+        "shortName" : {
+            "required" : true,
+            "rangelength" : [ 2, 50 ]
+        },
+        "code" : {
+            "rangelength" : [ 0, 50 ],
+            "alphanumericwithbasicpuncspaces" : true,
+            "notOnlyDigits" : false
+        }
     }
 };
