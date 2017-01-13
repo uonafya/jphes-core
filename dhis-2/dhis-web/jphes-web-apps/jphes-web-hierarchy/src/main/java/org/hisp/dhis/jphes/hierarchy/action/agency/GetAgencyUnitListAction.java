ec8f02b1d50a4ab9666e9c0225bc5fb0fd4c02ef
banga <dennisbanga@gmail.com>
2017-01-14 00:02:04 +0300

    CRUD method updates for Mechanism Unit and Agency Unit

--- dhis-2/dhis-web/jphes-web-apps/jphes-web-hierarchy/src/main/java/org/hisp/dhis/jphes/hierarchy/action/agency/GetAgencyUnitListAction.java
+++ dhis-2/dhis-web/jphes-web-apps/jphes-web-hierarchy/src/main/java/org/hisp/dhis/jphes/hierarchy/action/agency/GetAgencyUnitListAction.java
@@ -1,15 +1,65 @@
 package org.hisp.dhis.jphes.hierarchy.action.agency;
 
 import org.hisp.dhis.jphes.hierarchy.agency.AgencyUnit;
+import org.hisp.dhis.jphes.hierarchy.agency.AgencyUnitService;
 import org.hisp.dhis.paging.ActionPagingSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.apache.commons.lang.StringUtils.isNotBlank;
 
 /**
  * Created by @bangadennis on 05/01/17.
  */
 public class GetAgencyUnitListAction extends ActionPagingSupport<AgencyUnit>
 {
+    // -------------------------------------------------------------------------
+    // Dependencies
+    // -------------------------------------------------------------------------
+
+    @Autowired
+    private AgencyUnitService agencyUnitService;
+
+    // -------------------------------------------------------------------------
+    // Input/Output
+    // -------------------------------------------------------------------------
+
+    private List<AgencyUnit> agencyUnits;
+
+    public List<AgencyUnit> getAgencyUnits(){ return  agencyUnits; }
+
+    private String key;
+
+    public void setKey( String key )
+    {
+        this.key = key;
+    }
+
+    public String getKey()
+    {
+        return key;
+    }
+
+    // -------------------------------------------------------------------------
+    // Implementation
+    // -------------------------------------------------------------------------
     @Override public String execute() throws Exception
     {
+        // Filter on key only if set
+        if ( isNotBlank( key ) )
+        {
+            this.paging = createPaging(agencyUnitService.getAgencyUnitCountByName(key));
+
+            agencyUnits = agencyUnitService.getAgencyUnitsBetweenByName(key, paging.getStartPos(), paging.getPageSize() );
+        }
+        else
+        {
+            this.paging = createPaging( agencyUnitService.getAgencyUnitCount() );
+
+            agencyUnits = agencyUnitService.getAgencyUnitsBetween( paging.getStartPos(), paging.getPageSize() );
+        }
+
         return SUCCESS;
     }
 }
--- dhis-2/dhis-web/jphes-web-apps/jphes-web-hierarchy/src/main/java/org/hisp/dhis/jphes/hierarchy/action/mechanism/GetAgencyProgramListAction.java
+++ dhis-2/dhis-web/jphes-web-apps/jphes-web-hierarchy/src/main/java/org/hisp/dhis/jphes/hierarchy/action/mechanism/GetAgencyProgramListAction.java
@@ -1,15 +1,69 @@
 package org.hisp.dhis.jphes.hierarchy.action.mechanism;
 
+import org.hisp.dhis.jphes.hierarchy.agency.AgencyUnit;
+import org.hisp.dhis.jphes.hierarchy.agency.AgencyUnitService;
 import org.hisp.dhis.paging.ActionPagingSupport;
 import org.hisp.dhis.program.Program;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by afya on 13/01/17.
  */
 public class GetAgencyProgramListAction extends ActionPagingSupport<Program>
 {
-    @Override public String execute() throws Exception
+    // -------------------------------------------------------------------------
+    // Dependencies
+    // -------------------------------------------------------------------------
+
+    @Autowired
+    private AgencyUnitService agencyUnitService;
+
+    // -------------------------------------------------------------------------
+    // Input
+    // -------------------------------------------------------------------------
+
+    private Integer id;
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    // -------------------------------------------------------------------------
+    // Output
+    // -------------------------------------------------------------------------
+
+    private List<Program> programs;
+
+    public List<Program> getPrograms()
     {
+        return programs;
+    }
+
+    // -------------------------------------------------------------------------
+    // Action implementation
+    // -------------------------------------------------------------------------
+
+    @Override
+    public String execute()
+        throws Exception
+    {
+        AgencyUnit agencyUnit = agencyUnitService.getAgencyUnit( id );
+
+        programs = new ArrayList<>( agencyUnit.getPrograms() );
+
+        Collections.sort( programs );
+
+        if ( usePaging )
+        {
+            this.paging = createPaging( programs.size() );
+
+            programs = programs.subList( paging.getStartPos(), paging.getEndPos() );
+        }
+
         return SUCCESS;
     }
 }
