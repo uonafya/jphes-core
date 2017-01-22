ef8159fb2d24c2ffdce4ed8e46243656453b8704
banga <dennisbanga@gmail.com>
2017-01-22 23:18:10 +0300

    fixes

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/jphes/JphesProgramController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/jphes/JphesProgramController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping( value = JphesProgramSchemaDescriptor.API_ENDPOINT )
-public class ProgramController extends AbstractCrudController<Program>
+public class JphesProgramController extends AbstractCrudController<Program>
 {
 }
